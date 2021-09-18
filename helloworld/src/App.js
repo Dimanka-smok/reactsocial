9e8e9b25bb89c049844e8c945fae053dd25789dd
Dima <83983793+Dimanka-smok@users.noreply.github.com>
2021-09-18 11:24:03 +0300

    First commit

---
+++ helloworld/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Menu from './components/Menu/Menu';
+
+function App() {
+  return (
+    <div className="App">
+      <Menu/>
+      <h1>HelloWorld</h1>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ helloworld/src/components/Menu/Menu.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./Menu.css"
+
+function Menu(){
+    return (
+        <div className="Menu">
+            <a href="#">Главная</a>
+            <a href="#">Магазин</a>
+            <a href="#">Отзывы</a>
+            <a href="#">Доставка</a>
+        </div>
+    )
+}
+
+
+export default Menu
