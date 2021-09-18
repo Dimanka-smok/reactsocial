18ba1bf219a50bfefbb4911c903880f01f57fac8
Dima <83983793+Dimanka-smok@users.noreply.github.com>
2021-09-18 11:42:16 +0300

    Delete menu and footer components

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
-import Menu from './components/Menu/Menu';
 
 function App() {
   return (
     <div className="App">
-      <Menu/>
-      <h1>HelloWorld</h1>
       
     </div>
   );
