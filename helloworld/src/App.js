b5fefad69db8f65a50d0ec8bae13cdfaf6d62418
Dima <83983793+Dimanka-smok@users.noreply.github.com>
2021-09-20 19:03:09 +0300

    Create Section

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Navbar from "./components/Navbar/Navbar"
+import Section from "./components/Section/Section"
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar/> 
+      <Section/>
     </div>
   );
 }
 
 export default App;
+  
