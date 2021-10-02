f3d4294d249557cc4e3e6d748ed1f32f1eedb097
Dima <83983793+Dimanka-smok@users.noreply.github.com>
2021-10-02 11:53:50 +0300

    Add mapping

--- helloworld/src/components/Dialogs/Dialog/Message.js
+++ helloworld/src/components/Dialogs/Dialog/Message.js
@@ -4,9 +4,6 @@ import "./Message.css"
 function Message (props){
     return(
         <div className="messsage">
-            <div className="justify-content-end">
-            <h3>{props.name} </h3>
-            </div>
             <p className="message-text">{props.message}</p>
         </div>
     )
--- helloworld/src/components/Dialogs/Dialogs.js
+++ helloworld/src/components/Dialogs/Dialogs.js
@@ -4,24 +4,37 @@ import Message from "./Dialog/Message";
 import { NavLink } from "react-router-dom";
 import DialogItem from "./DialogsItems/DialogsItem";
 
+let dialogNames=[
+    {name: "Владимир", id: "1"},
+    {name: "Мария", id: "2"},
+    {name: "Михаил", id: "3"},
+    {name: "Вячеслав", id: "4"},  
+    {name: "Алексей", id: "5"}, 
+]
+
+let messages=[
+    {message: "Привет. Как дела?", id: "1"},
+    {message: "Здравствуйте.", id: "2"},
+    {message: "Здарова. Пойдешь гулять?", id: "3"},
+    {message: "Привет. Ты вечером свободен?", id: "4"},
+]
+
 function Dialogs(){
-    let message1="Не следует, однако, забывать о том, что консультация с профессионалами из IT позволяет выполнить важнейшие задания по разработке дальнейших направлений развитая системы массового участия. С другой стороны выбранный нами инновационный."
-    let message2="Дорогие друзья, постоянный количественный рост и сфера нашей активности способствует подготовке и реализации направлений прогрессивного развития. Практический опыт показывает, что реализация намеченного плана развития влечет за собой процесс внедрения."
-    let message3="С другой стороны начало повседневной работы по формированию позиции позволяет оценить значение существующих финансовых и административных условий."
-    let message4="С другой стороны реализация намеченного плана развития требует от нас анализа существующих финансовых и административных условий."
     return(
         <div className="dialogs">
             <div className="dialog-user">
-               <DialogItem name={"Владимир"} id={"1"}/>
-               <DialogItem name={"Мария"} id={"2"}/>
-               <DialogItem name={"Михаил"} id={"3"}/>
-               <DialogItem name={"Вячеслав"} id={"4"}/>
+                {
+                    dialogNames.map((object)=><DialogItem name={object.name} id={object.id}/>)
+                }
             </div>
             <div className="dialog-text">
-                <Message  message={message1}/>
-                <Message  message={message2}/>
-                <Message  message={message3}/>
-                <Message message={message4}/>
+                {
+                    messages.map((object)=> <Message message={object.message} id={object.id}/>)
+                }
+                <div className="addMessage">
+                    <input placeholder="Напишите сообщение..." type="text"/>
+                    <button>Отправить</button>
+                </div>
             </div>
         </div>
     )
